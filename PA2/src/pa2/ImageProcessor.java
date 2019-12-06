31307e679be11af5b4e0c86e71458a1e61963ae3
Lorenzo Zenitsky <lorenzoz@iastate.edu>
2019-12-05 18:59:23 -0600

    finished reduce width

--- PA2/src/pa2/ImageProcessor.java
+++ PA2/src/pa2/ImageProcessor.java
@@ -1,5 +1,7 @@
 package pa2;
 
+import java.util.ArrayList;
+
 /**
  * 
  * @author Lorenzo Zenitsky, Gabrielle Johnston
@@ -7,58 +9,89 @@ package pa2;
  */
 
 public class ImageProcessor {
-	private static Picture picture = null;
+	private static Picture picture;
+	private static Pixel[][] M;
 	
 	static Picture reduceWidth(int x, String inputImage) {
 		picture = new Picture(inputImage);
+		fillImageMatrix();
 		
-		return picture;
-	}
-	
-	private int dist(int[] p, int[] q) {
-		if(p.length > 3 || q.length > 3) {
-			return -1;
+		for(int i = 0; i < x; i++) {
+			int[][] importanceArr = new int[picture.height()][picture.width() - i];
+			findImportance(importanceArr);
+			ArrayList<Tuple> minCut = MatrixCuts.widthCut(importanceArr);
+			for(int j = 0; j < picture.height(); j++) {
+				M[j][minCut.get(j).getY()] = null;
+			}
 		}
 		
-		int x = p[0] - q[0];
-		int y = p[1] - q[1];
-		int z = p[2] - q[2];
+		return picFromMatrix(x);
+	}
+	
+	private static void fillImageMatrix() {
+		for(int i = 0; i < M.length; i++) {
+			for(int j = 0; j < M[0].length; j++) {
+				int rgb[] = getRGBValues(picture.getRGB(j, i));
+				M[i][j] = new Pixel(rgb[0], rgb[1], rgb[2]);
+			}
+		}
+	}
+	
+	private static Picture picFromMatrix(int x) {
+		Picture newPic = new Picture(picture.width() - x, picture.height());
+		
+		for(int i = 0; i < M.length; i++) {
+			for(int j = 0; j < M[0].length; j++) {
+				if(M[i][j] != null) {
+					newPic.setRGB(j, i, getRGBValuesFromPixel(M[i][j]));
+				}
+			}
+		}
+		return newPic;
+	}
+	
+	private static void findImportance(int[][] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[0].length; j++) {
+				arr[i][j] = importance(i, j);
+			}
+		}
+	}
+	
+	private static int dist(Pixel p, Pixel q) {
+		int x = p.getR() - q.getR();
+		int y = p.getG() - q.getG();
+		int z = p.getB() - q.getB();
 		return (x * x) + (y * y) + (z * z);
 	}
 	
-	private int importance(int row, int col) { // possibly need to check if column is less than 0 or more than the width - 1
-		int rgb = picture.getRGB(col, row);
-		int r = (rgb >> 16) & 0xff;
-		int g = (rgb >> 8) & 0xff;
-		int b = (rgb) & 0xff;
-		int[] og = { r, g, b };
+	private static int importance(int row, int col) { // possibly need to check if column is less than 0 or more than the width - 1
+		Pixel og = M[row][col];
 		
 		if(col == 0) {
-			int[] q = getRGBValues(picture.getRGB(col + 1, row));
-			
-			return dist(og, q);
+			return dist(og, M[row][col + 1]);
 		}
 		else if(col == picture.width() - 1) {
-			int[] q = getRGBValues(picture.getRGB(col - 1, row));
-			
-			return dist(og, q);
+			return dist(og, M[row][col - 1]);
 		}
 		else {
-			int[] p = getRGBValues(picture.getRGB(col - 1, row));
-			int[] q = getRGBValues(picture.getRGB(col + 1, row));
-			
-			return dist(p, q);
+			return dist(M[row][col - 1], M[row][col + 1]);
 		}
 		
 	}
 	
-	private int[] getRGBValues(int rgb) {
+	private static int[] getRGBValues(int rgb) {
 		int[] values = new int[3];
 		
 		values[0] = (rgb >> 16) & 0xff;
 		values[1] = (rgb >> 8) & 0xff;
-		values[2] = (rgb) & 0xff;
+		values[2] = (rgb) & 0xff; // or (rgb >> 0)
 		
 		return values;
+	}
+	
+	private static int getRGBValuesFromPixel(Pixel p) {
+		int rgb = p.getR() + p.getG() + p.getB();
+		return rgb;
 	}
 }
---
+++ PA2/src/pa2/Pixel.java
@@ -0,0 +1,34 @@
+package pa2;
+
+public class Pixel {
+	private int r;
+	private int g;
+	private int b;
+	
+	public Pixel(int r, int g, int b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+	
+	public int[] get() {
+		int[] rbg = new int[3];
+		rbg[0] = r;
+		rbg[1] = b;
+		rbg[2] = g;
+		return rbg;
+	}
+	
+	public int getR() {
+		return r;
+	}
+	
+	public int getG() {
+		return g;
+	}
+	
+	public int getB() {
+		return b;
+	}
+
+}
